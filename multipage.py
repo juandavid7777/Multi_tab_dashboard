864050a5d23bf37ec5aaec7bd867bc568f06c714
juandavid7777 <juandavid7777@gmail.com>
2022-03-05 16:53:28 +0400

    Changed main page display

--- multipage.py
+++ multipage.py
@@ -33,7 +33,7 @@ class MultiPage:
     def run(self):
         # Drodown to select the page to run  
         page = st.sidebar.selectbox(
-            'App Navigation', 
+            'Metric Types', 
             self.pages, 
             format_func=lambda page: page['title']
         )
