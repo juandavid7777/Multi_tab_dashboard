98f957343f206cd30fbabfed256322e236aeab78
juandavid7777 <juandavid7777@gmail.com>
2022-03-05 17:19:16 +0400

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -5,10 +5,10 @@ from pages import utils
 
 # @st.cache
 def app():
-    st.markdown("Metrics latest summary")
-
+    st.markdown("# Metrics latest summary")
+    st.write("\n")
     # Summarizes some metrics
-    st.markdown("BTC price")
+    st.markdown("# BTC price")
     st.write("\n")
     st.markdown("Technical summary") 
     st.markdown("Onchain summary") 
