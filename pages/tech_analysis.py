a793fb4e0be84fe08b7e77335ad4a4cac59175f6
juandavid7777 <juandavid7777@gmail.com>
2022-03-06 10:54:58 +0400

    Added price graphs

--- pages/tech_analysis.py
+++ pages/tech_analysis.py
@@ -2,6 +2,10 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
+from datetime import datetime
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
 
 # @st.cache
 def app():
@@ -9,6 +13,38 @@ def app():
     st.write("\n")
     # Summarizes some metrics
 
-    st.markdown("Technical summary") 
-    st.markdown("Onchain summary") 
-    st.markdown("Sentiment summary")  
+    #1.-----Downloads data
+    df = pd.read_csv("https://raw.githubusercontent.com/juandavid7777/BTC_risk_metric/891a1032cb63160763c33f235a4714f2083c9713/BTC_price_projections.csv")
+
+
+    #2.-----API token definition
+    coin_name = "BTC"
+    projected_days = 180
+
+    #3.-----Plots figures
+
+    #=================================================== BANDS CHART===========================================
+    fig = go.Figure()
+
+    #Price candlesticks plots
+    fig.add_trace(go.Scatter(
+        x=df['Date'],
+        y=df["close"],
+        mode = 'lines',
+        name = '',
+        line = dict(width = 0.5, color = "white")
+        ))
+
+    fig.add_trace(go.Candlestick(
+        x=df['Date'],
+        open=df['open'],
+        high=df['high'],
+        low=df['low'],
+        close=df['close'],
+        name = coin_name + ' price'
+        ))
+
+    fig.update_xaxes(showgrid=True, gridwidth=1, gridcolor='grey')
+    fig.update_yaxes(showgrid=True, gridwidth=1, gridcolor='grey')
+
+    st.plotly_chart(fig)
