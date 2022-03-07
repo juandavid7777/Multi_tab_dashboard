c02567db99dcd55fd451fc52856a8c578d8583cc
juandavid7777 <juandavid7777@gmail.com>
2022-03-07 14:50:36 +0400

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -49,13 +49,15 @@ def app():
     df_api = df_api24[[ "Unnamed: 0",
                         "/v1/metrics/market/mvrv_z_score",
                         "/v1/metrics/mining/marketcap_thermocap_ratio",
-                        "/v1/metrics/indicators/net_unrealized_profit_loss"]]
+                        "/v1/metrics/indicators/net_unrealized_profit_loss",
+                        "/v1/metrics/indicators/puell_multiple"]]
 
     #Renames the metrics
     df_api = df_api.rename(columns={"Unnamed: 0":"date",
                         "/v1/metrics/market/mvrv_z_score":"MVRV Z Score",
                         "/v1/metrics/mining/marketcap_thermocap_ratio":"Thermocap Ratio",
-                        "/v1/metrics/indicators/net_unrealized_profit_loss":"NUPL"})
+                        "/v1/metrics/indicators/net_unrealized_profit_loss":"NUPL",
+                        "/v1/metrics/indicators/puell_multiple":"Puell Multiple"})
 
     st.table(df_api.set_index("date").iloc[-2])
 
