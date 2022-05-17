310dc47990a632c8ba449d46639a359d5bc0e1cb
juandavid7777 <juandavid7777@gmail.com>
2022-05-17 18:03:16 +0400

    Added bars

--- pages/home.py
+++ pages/home.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-
+import plotly.graph_objects as go
+from functions import add_metricBar
 
 # @st.cache
 def app():
@@ -108,6 +109,53 @@ def app():
                         "/v1/metrics/indicators/puell_multiple":"Puell Multiple"})
 
     st.table(df_api.set_index("date").iloc[-2])
+
+
+    #test!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    #Dataframe cosntructed externally (APIS or metrics)
+
+    #Collected data API/metrics
+    values = [1.1429,0.000005,0.3936,0.8243,0.55,0.0015,0.13,0.35]
+    titles = ["MVRV Z Score","Thermocap Ratio","NUPL","Puell Multiple", "Percent supply profit", "Reserve risk", "MA log relation", "Time channel"]
+    df_IDs = ["name1","name2","name3","name4", "name5", "name6", "name7", "name8"]
+    values_prev = [1.14,0.000004,0.32,0.8245,0.52,0.0016, 0.13, 0.36]
+
+    #Researched data
+    min_vals = [-1,0,-1.5,0.2,0,0.0005,0,0]
+    max_vals = [12,0.000007,1,10,1,0.06,1,1]
+    buy_bnds = [0,0.00000033,0,0.5,0.5,0.003,0.2,0.2]
+    mid_bnds = [3.5,0.000002165,0.375,2.25,0.75,0.01,0.5,0.5]
+    sell_bnds = [7,0.000004,0.75,4,0.95,0.02,0.8,0.8]
+
+    df = pd.DataFrame(data = {"value":values,
+                            "title":titles,
+                            "df_ID":df_IDs,
+                            "value_prev":values_prev,
+                            "min_val":min_vals,
+                            "max_val":max_vals,
+                            "buy_bnd":buy_bnds,
+                            "mid_bnd":mid_bnds,
+                            "sell_bnd":sell_bnds})
+
+    #Plotting range            
+    fig = go.Figure()
+
+    #Plotting-----------------------------------------
+
+    nos_metrics = len(df)
+    msp = 0.06
+    sp = (1-(msp*nos_metrics))/nos_metrics
+
+    for i, row in df.iterrows():
+        add_metricBar(value = row["value"], title = row["title"], value_prev = row["value_prev"], min_val = row["min_val"], max_val = row["max_val"], buy_bnd = row["buy_bnd"], mid_bnd =row["mid_bnd"], sell_bnd = row["sell_bnd"], x = [0,1], y = [i*(msp+sp),i*(msp+sp)+sp])
+
+    fig.update_layout(height = nos_metrics*60 , margin = {'t':0, 'b':50, 'l':300})
+
+    fig.show()
+
+
+    #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
 
     #On chain summary end ======================================================================================================================================= 
     #Sentiment summary ======================================================================================================================================= 
--- pages/tech_analysis.py
+++ pages/tech_analysis.py
@@ -26,7 +26,6 @@ def app():
                         ]]
 
     df_metrics = df_metrics24[[ "Date",
-                        'close',
                         'price_reg',
                         'plus_3STDV',
                         'minus_3STDV',
@@ -34,8 +33,6 @@ def app():
                         'minus_2STDV',
                         'plus_1STDV',
                         'minus_1STDV',
-                        "norm_dist",
-                        "risk_MA_norm"
                         ]]
     
     #Renames the metrics
@@ -46,13 +43,13 @@ def app():
                         "/v1/metrics/market/price_usd_ohlc-l":"low",
                         })
 
-    df_metrics = df_metrics.rename(columns={"Date":"date"})
+    df_metrics = df_metrics24.rename(columns={"Date":"date"})
 
     #2.-----API token definition
     coin_name = "BTC"
     projected_days = 180
 
-    #3.-----Plots figures Uncertainity bands=======================================
+    #3.-----Plots figures Uncertainity bands
     #==== Basic candel stick chart =================================================
     fig = go.Figure()
 
@@ -161,111 +158,57 @@ def app():
     st.plotly_chart(fig, use_container_width = True)
 
     # Uncertainity bands plot finished ===================================================
-    #4=== Colored chart risk metric reg ==================================================
 
+    #=== Risk metric colored ========================================
+    #1.-----Downloads data and cleans it
+    df_api24 = pd.read_csv("https://raw.githubusercontent.com/juandavid7777/Multi_tab_dashboard/main/data/api_24h.csv?token=GHSAT0AAAAAABSGJ422NL7BZDYFQTJXHFPCYRENPLQ")
+    df_metrics24 = pd.read_csv("https://raw.githubusercontent.com/juandavid7777/Multi_tab_dashboard/main/data/metrics24.csv")
+
+    #Selects metrics
+    df_api = df_api24[[ "Unnamed: 0",
+                        "/v1/metrics/market/price_usd_ohlc-o",
+                        "/v1/metrics/market/price_usd_ohlc-h",
+                        "/v1/metrics/market/price_usd_ohlc-c",
+                        "/v1/metrics/market/price_usd_ohlc-l",
+                        ]]
+
+    df_metrics = df_metrics24[[ "Date",
+                        'price_reg',
+                        'plus_3STDV',
+                        'minus_3STDV',
+                        'plus_2STDV',
+                        'minus_2STDV',
+                        'plus_1STDV',
+                        'minus_1STDV',
+                        ]]
+    
+    #Renames the metrics
+    df_api = df_api.rename(columns={"Unnamed: 0":"date",
+                        "/v1/metrics/market/price_usd_ohlc-o":"open",
+                        "/v1/metrics/market/price_usd_ohlc-h":"high",
+                        "/v1/metrics/market/price_usd_ohlc-c":"close",
+                        "/v1/metrics/market/price_usd_ohlc-l":"low",
+                        })
+
+    df_metrics = df_metrics24.rename(columns={"Date":"date"})
+    
+    
     fig = go.Figure()
 
     # Create figure with secondary y-axis
     fig = make_subplots(specs=[[{"secondary_y": True}]])
 
     fig.add_trace(go.Scatter(
-        x=df_metrics['date'],
-        y=df_metrics["close"],
-        mode = 'markers',
-        name = '',
-        marker=dict(size=3,color = df_metrics["norm_dist"], colorscale='Jet',showscale=True)
-        ),secondary_y=False)
-
-    #Defines figure properties
-    fig.update_layout(
-        title = coin_name + " time uncertainity metric",
-        xaxis_title= "Date",
-        yaxis_title= coin_name + " price (USD)",
-        
-        plot_bgcolor = "black",
-        yaxis_type="log",
-        xaxis_rangeslider_visible=False)
-
-    fig.update_xaxes(showgrid=True, gridwidth=1, gridcolor='grey')
-    fig.update_yaxes(showgrid=True, gridwidth=1, gridcolor='grey')
-
-    st.plotly_chart(fig)
-
-    # End of colored chart risk metric reg ==============================================
-    #5=== Colored chart MA risk metric ==================================================
-
-    fig = go.Figure()
-
-    # Create figure with secondary y-axis
-    fig = make_subplots(specs=[[{"secondary_y": True}]])
-
-    fig.add_trace(go.Scatter(
-        x=df_metrics['date'],
-        y=df_metrics["close"],
-        mode = 'markers',
-        name = '',
-        marker=dict(size=3,color = df_metrics["risk_MA_norm"], colorscale='Jet',showscale=True)
-        ),secondary_y=False)
-
-    #Defines figure properties
-    fig.update_layout(
-        title = coin_name + " MA uncertainity metric",
-        xaxis_title= "Date",
-        yaxis_title= coin_name + " price (USD)",
-        
-        plot_bgcolor = "black",
-        yaxis_type="log",
-        xaxis_rangeslider_visible=False)
-
-    fig.update_xaxes(showgrid=True, gridwidth=1, gridcolor='grey')
-    fig.update_yaxes(showgrid=True, gridwidth=1, gridcolor='grey')
-
-    st.plotly_chart(fig)
-
-    # End of colored chart MA risk metric ==============================================
-    # Combined Basic candel stick chart =================================================
-    # Create figure with secondary y-axis
-    fig = make_subplots(specs=[[{"secondary_y": True}]])
-
-    #Price candlesticks plots
-    fig.add_trace(go.Scatter(
-        x=df_metrics['date'],
-        y=df_metrics["close"],
-        mode = 'lines',
-        name = '',
-        line = dict(width = 0.5, color = "white")
-        ),secondary_y=False)
-
-    fig.add_trace(go.Candlestick(
         x=df_api['date'],
-        open=df_api['open'],
-        high=df_api['high'],
-        low=df_api['low'],
-        close=df_api['close'],
-        name = coin_name + ' price'
+        y=df_api["close"],
+        mode = 'markers',
+        name = '',
+        marker=dict(size=3,color = df_api[metric_new_name], colorscale='Jet',showscale=True)
         ),secondary_y=False)
-
-    #Adds metric
-    fig.add_trace(go.Scatter(
-        x=df_metrics['date'],
-        y=df_metrics["norm_dist"],
-        mode = 'lines',
-        name = "Time uncertainity downside risk",
-        line = dict(width = 1.0, color = "chartreuse")
-        ),secondary_y=True)
-
-    #Adds metric
-    fig.add_trace(go.Scatter(
-        x=df_metrics['date'],
-        y=df_metrics["risk_MA_norm"],
-        mode = 'lines',
-        name = "MA uncertainity downside risk",
-        line = dict(width = 1.0, color = "cyan")
-        ),secondary_y=True)
 
     #Defines figure properties
     fig.update_layout(
-        title = "Combined downside risk metrics",
+        title = coin_name + " Fear and Greed Index",
         xaxis_title= "Date",
         yaxis_title= coin_name + " price (USD)",
         
@@ -274,9 +217,40 @@ def app():
         xaxis_rangeslider_visible=False)
 
     fig.update_xaxes(showgrid=True, gridwidth=1, gridcolor='grey')
-    fig.update_yaxes(showgrid=True, gridwidth=1, gridcolor='grey', secondary_y = False)
-    fig.update_yaxes(title = "Risk metric (0 - 1)", showgrid=True, gridwidth=1, gridcolor='silver', secondary_y = True)
+    fig.update_yaxes(showgrid=True, gridwidth=1, gridcolor='grey')
 
     st.plotly_chart(fig)
+    #Frea and Greed raw end ======================================================================
 
-    # # combined END ===================================================================================
+
+
+
+    # #=== Colored chart ========================================
+
+    # fig = go.Figure()
+
+    # # Create figure with secondary y-axis
+    # fig = make_subplots(specs=[[{"secondary_y": True}]])
+
+    # fig.add_trace(go.Scatter(
+    #     x=df['Date'],
+    #     y=df["close"],
+    #     mode = 'markers',
+    #     name = '',
+    #     marker=dict(size=3,color = df["norm_dist"], colorscale='Jet',showscale=True)
+    #     ),secondary_y=False)
+
+    #Defines figure properties
+    # fig.update_layout(
+    #     title = coin_name + " uncertainity colored metric",
+    #     xaxis_title= "Date",
+    #     yaxis_title= coin_name + " price (USD)",
+        
+    #     plot_bgcolor = "black",
+    #     yaxis_type="log",
+    #     xaxis_rangeslider_visible=False)
+
+    # fig.update_xaxes(showgrid=True, gridwidth=1, gridcolor='grey')
+    # fig.update_yaxes(showgrid=True, gridwidth=1, gridcolor='grey')
+
+    # st.plotly_chart(fig)
